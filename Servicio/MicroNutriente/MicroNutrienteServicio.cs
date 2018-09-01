69fa0b6eefae352f245dff09c7d4c690b4f1d4a8
Sebastian H <sebahomsi@gmail.com>
2018-09-01 12:22:08 -0300

    seba rules

---
+++ Servicio/MicroNutriente/MicroNutrienteServicio.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Servicio.Interface.Alimento;
+using Servicio.Interface.MicroNutriente;
+
+namespace Servicio.MicroNutriente
+{
+    public class MicroNutrienteServicio: ServicioBase, IMicroNutrienteServicio
+    {
+        public async Task<long> Add(MicroNutrienteDto dto)
+        {
+            var micro = new Dominio.Entidades.MicroNutriente()
+            {
+                Codigo = dto.Codigo,
+                Descripcion = dto.Descripcion,
+                Eliminado = false
+            };
+
+            Context.MicroNutrientes.Add(micro);
+            await Context.SaveChangesAsync();
+            return micro.Id;
+        }
+
+        public async Task Update(MicroNutrienteDto dto)
+        {
+            var micro = Context.MicroNutrientes.Find(dto.Id);
+            if (micro == null) throw new ArgumentNullException();
+
+            micro.Codigo = dto.Codigo;
+            micro.Descripcion = dto.Descripcion;
+            micro.Eliminado = dto.Eliminado;
+
+            await Context.SaveChangesAsync();
+        }
+
+        public async Task Delete(long id)
+        {
+            var micro = Context.MicroNutrientes.Find(id);
+            if (micro == null) throw new ArgumentNullException();
+
+            micro.Eliminado = !micro.Eliminado;
+
+            await Context.SaveChangesAsync();
+        }
+
+        public async Task<ICollection<MicroNutrienteDto>> Get(string cadenaBuscar)
+        {
+            int.TryParse(cadenaBuscar, out var codigo);
+            return await Context.MicroNutrientes
+                .AsNoTracking()
+                .Where(x => x.Descripcion.Contains(cadenaBuscar) || x.Codigo == codigo)
+                .Select(x => new MicroNutrienteDto()
+                {
+                    Id = x.Id,
+                    Codigo = x.Codigo,
+                    Descripcion = x.Descripcion,
+                    Eliminado = x.Eliminado
+                }).ToListAsync();
+        }
+
+        public async Task<MicroNutrienteDto> GetById(long id)
+        {
+            var micro = await Context.MicroNutrientes
+                .AsNoTracking()
+                .Include("Alimentos")
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (micro == null) throw new ArgumentNullException();
+
+            return new MicroNutrienteDto()
+            {
+                Id = micro.Id,
+                Codigo = micro.Codigo,
+                Descripcion = micro.Descripcion,
+                Eliminado = micro.Eliminado,
+                Alimentos = micro.Alimentos.Select(x=> new AlimentoDto()
+                {
+                    Id = x.Id,
+                    Codigo = x.Codigo,
+                    Descripcion = x.Descripcion,
+                    SubGrupoId = x.SubGrupoId,
+                    MacroNutrienteId = x.MacroNutrienteId,
+                    TieneMacroNutriente = x.TieneMacroNutriente,
+                    Eliminado = x.Eliminado
+                }).ToList()
+            };
+        }
+    }
+}
---
+++ Servicio/MicroNutrienteDetalle/MicroNutrienteDetalleServicio.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Servicio.Interface.MicroNutrienteDetalle;
+
+namespace Servicio.MicroNutrienteDetalle
+{
+    public class MicroNutrienteDetalleServicio : ServicioBase, IMicroNutrienteDetalleServicio
+    {
+        public async Task<long> Add(MicroNutrienteDetalleDto dto)
+        {
+            var micro = new Dominio.Entidades.MicroNutrienteDetalle()
+            {
+                Codigo = dto.Codigo,
+                AlimentoId = dto.AlimentoId,
+                MicroNutrienteId = dto.MicroNutrienteId,
+                Cantidad = dto.Cantidad,
+                Unidad = dto.Unidad
+            };
+
+            Context.MicroNutrienteDetalles.Add(micro);
+            await Context.SaveChangesAsync();
+            return micro.Id;
+        }
+
+        public async Task Update(MicroNutrienteDetalleDto dto)
+        {
+            var micro = Context.MicroNutrienteDetalles.Find(dto.Id);
+            if (micro == null) throw new ArgumentNullException();
+
+            micro.Codigo = dto.Codigo;
+            micro.AlimentoId = dto.AlimentoId;
+            micro.MicroNutrienteId = dto.MicroNutrienteId;
+            micro.Cantidad = dto.Cantidad;
+            micro.Unidad = dto.Unidad;
+
+            await Context.SaveChangesAsync();
+        }
+
+        public async Task Delete(long id)
+        {
+            var detalle = Context.MicroNutrienteDetalles.Find(id);
+            Context.MicroNutrienteDetalles.Remove(detalle);
+            await Context.SaveChangesAsync();
+        }
+
+        public async Task<ICollection<MicroNutrienteDetalleDto>> Get(string cadenaBuscar)
+        {
+            int.TryParse(cadenaBuscar, out var codigo);
+            return await Context.MicroNutrienteDetalles
+                .AsNoTracking()
+                .Include("Alimento")
+                .Include("MicroNutriente")
+                .Where(x => x.Codigo == codigo /*|| x.Alimento.Descripcion.Contains(cadenaBuscar)*/)
+                .Select(x => new MicroNutrienteDetalleDto()
+                {
+                    Id = x.Id,
+                    Codigo = x.Codigo,
+                    AlimentoId = x.AlimentoId,
+                    AlimentoStr = x.Alimento.Descripcion,
+                    MicroNutrienteId = x.MicroNutrienteId,
+                    MicroNutrienteStr = x.MicroNutriente.Descripcion,
+                    Cantidad = x.Cantidad,
+                    Unidad = x.Unidad
+                }).ToListAsync();
+        }
+
+        public async Task<MicroNutrienteDetalleDto> GetById(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
