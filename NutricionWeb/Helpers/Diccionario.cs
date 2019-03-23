c2c0b2bfba27db5e3e50de329a4a8519eccf312c
alejandro lazarte <ale_sl_99@hotmail.com>
2019-03-23 19:06:55 -0300

    Prueba

---
+++ NutricionWeb/Helpers/Diccionario.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace NutricionWeb.Helpers
+{
+    public static class Diccionario
+    {
+        private static readonly IDictionary<int, string> Comidas;
+        private static readonly IDictionary<int, string> Colores;
+
+        static Diccionario()
+        {
+            Comidas = new Dictionary<int, string>
+            {
+                {1, "Desayuno"},
+                {2, "Media Mañana"},
+                {3, "Almuerzo"},
+                {4, "Merienda"},
+                {5, "Media Tarde"},
+                {6, "Cena"}
+            };
+
+            Colores = new Dictionary<int, string>
+            {
+                {1, "#ffc0d7"},
+                {2, "#c1ffc0"},
+                {3, "#c0c5ff"},
+                {4, "#ffc9c0"},
+                {5, "#dbffc0"},
+                {6, "#fbffc0"}
+            };
+
+
+        }
+
+        public static string ObtenerComidaStr(int numero)
+        {
+            return Comidas[numero];
+        }
+
+        public static string ObtenerComidaColor(int numero)
+        {
+            return Colores[numero];
+        }
+
+    }
+}
--- Servicio/Comida/ComidaServicio.cs
+++ Servicio/Comida/ComidaServicio.cs
@@ -1,11 +1,10 @@
-﻿using System;
+﻿using Servicio.Interface.Comida;
+using Servicio.Interface.Opcion;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Servicio.Interface.Comida;
-using Servicio.Interface.Opcion;
 
 namespace Servicio.Comida
 {
