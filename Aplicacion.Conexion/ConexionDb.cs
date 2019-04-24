f5aa26737a49f2ca9b776b1a4bfe981ccc54a4ac
Ezequiel <ezequieltosi.96@hotmail.com>
2019-04-24 16:19:30 -0300

    asdfghj

--- Aplicacion.Conexion/ConexionDb.cs
+++ Aplicacion.Conexion/ConexionDb.cs
@@ -2,10 +2,10 @@
 {
     public static class ConexionDb
     {
-        private const string Servidor = @"SEBAHOMSI\SQLEXPRESS";
+        private const string Servidor = @"DESKTOP-TTT7KKD";
         private const string BaseDatos = "Nutricion0039";
         private const string Usuario = "sa";
-        private const string Password = "dumas123";
+        private const string Password = "pa$$word";
 
         public static string ObtenerCadenaConexion => $"Data Source={Servidor}; Initial Catalog={BaseDatos}; User Id={Usuario}; Password={Password}";
     }
