7abfa1c174195548b799ac639d8a9f772f194aaf
alejandro lazarte <ale_sl_99@hotmail.com>
2019-04-25 20:07:53 +0200

    chrt js

---
+++ NutricionWeb/Controllers/Estadisticas/EstadisticasController.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace NutricionWeb.Controllers.Estadisticas
+{
+    public class EstadisticasController : Controller
+    {
+        // GET: Estadisticas
+        public async Task<ActionResult> Index(long? id)
+        {
+            return PartialView();
+        }
+    }
+}
