b31878f9754d33d226e1d3e87858d18474675c8f
seba homsi <sebahomsi@gmail.com>
2019-04-28 22:35:21 -0300

    pdf plan vista

--- Aplicacion.Conexion/ConexionDb.cs
+++ Aplicacion.Conexion/ConexionDb.cs
@@ -2,8 +2,8 @@
 {
     public static class ConexionDb
     {
-        private const string Servidor = @"SEBAHOMSI\SQLEXPRESS";
-        private const string BaseDatos = "Nutricion0040";
+        private const string Servidor = @"DESKTOP-MH85TLT\SEBA";
+        private const string BaseDatos = "Nutricion00a1";
         private const string Usuario = "sa";
         private const string Password = "dumas123";
 
