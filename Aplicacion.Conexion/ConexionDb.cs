cdca038fb5629cd43fdf80800db71692e007c06e
seba homsi <sebahomsi@gmail.com>
2019-05-16 10:52:11 -0300

    modal comida acomodado

--- Aplicacion.Conexion/ConexionDb.cs
+++ Aplicacion.Conexion/ConexionDb.cs
@@ -2,10 +2,10 @@
 {
     public static class ConexionDb
     {
-        private const string Servidor = @"DESKTOP-SBL9H94\SQLEXPRESS";
+        private const string Servidor = @"DESKTOP-MH85TLT\SEBA";
         private const string BaseDatos = "Nutricion02";
         private const string Usuario = "sa";
-        private const string Password = "hola123";
+        private const string Password = "dumas123";
 
         public static string ObtenerCadenaConexion => $"Data Source={Servidor}; Initial Catalog={BaseDatos}; User Id={Usuario}; Password={Password}";
     }
