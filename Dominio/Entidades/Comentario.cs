843ed08aea28c4ac67765b21dc7a299d29c40954
l.277 <l.277@hotmail.com>
2019-06-07 17:31:24 -0300

    Comentario+Servicio :D

---
+++ Dominio/Entidades/Comentario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entidades
+{
+    [Table("Comentario")]    
+    public class Comentario :EntidadBase
+    {
+        public long ComidaId { get; set; }
+
+        public long PlanId { get; set; }
+
+        public long DiaId { get; set; }
+
+        public string Texto { get; set; }
+
+        public ICollection<Opcion> Opciones { get; set; }
+    }
+}
