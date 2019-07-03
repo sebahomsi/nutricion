01b4e0250d2f28edbbe7697fa3eba671fc0620b5
seba homsi <sebahomsi@gmail.com>
2019-07-03 17:15:28 -0300

    aun mas parciales

--- NutricionWeb/Controllers/Turno/TurnoController.cs
+++ NutricionWeb/Controllers/Turno/TurnoController.cs
@@ -228,6 +228,79 @@ namespace NutricionWeb.Controllers.Turno
             return volver == 0 || volver == null ? RedirectToAction("Index") : RedirectToAction("About", "Home");
         }
 
+        public async Task<ActionResult> EditParcial(long? id, int? volver)
+        {
+            ViewBag.Volver = volver ?? 0;
+
+            if (id == null) return RedirectToAction("Error", "Home");
+
+            var turno = await _turnoServicio.GetById(id.Value);
+
+            return PartialView(new TurnoABMViewModel()
+            {
+                Id = turno.Id,
+                PacienteId = turno.PacienteId,
+                PacienteStr = turno.PacienteStr,
+                HorarioEntradaDateTime = turno.HorarioEntrada,
+                HorarioSalidaDateTime = turno.HorarioSalida,
+                HorarioEntrada = turno.HorarioEntrada.ToString("dd/MM/yyyy HH:mm"),
+                HorarioSalida = turno.HorarioSalida.ToString("dd/MM/yyyy HH:mm"),
+                Motivo = turno.Motivo,
+                Numero = turno.Numero,
+                Eliminado = turno.Eliminado,
+                EstadoId = turno.EstadoId,
+                EstadoDescripcion = turno.EstadoDescripcion
+            });
+        }
+
+        // POST: Turno/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditParcial(TurnoABMViewModel vm, int? volver)
+        {
+            var establecimientoId = ObtenerEstablecimientoIdUser();
+            ViewBag.Volver = volver;
+            try
+            {
+                vm = ModificarFechas(vm);
+                if (ModelState.IsValid)
+                {
+                    if (vm.HorarioEntradaDateTime > vm.HorarioSalidaDateTime)
+                    {
+                        ModelState.AddModelError(string.Empty, "El campo Horario de Entrada no puede ser mayor al de Salida");
+
+                        return PartialView(vm);
+                    }
+
+                    var turnos = await _turnoServicio.Get(establecimientoId, false, string.Empty);
+
+                    foreach (var turno in turnos)
+                    {
+                        if (turno.HorarioEntrada.Date == vm.HorarioEntradaDateTime.Date)
+                        {
+                            if (turno.HorarioEntrada.TimeOfDay < vm.HorarioEntradaDateTime.TimeOfDay &&
+                                vm.HorarioEntradaDateTime.TimeOfDay < turno.HorarioSalida.TimeOfDay)
+                            {
+                                ModelState.AddModelError(string.Empty, "Ya existe un turno para ese rango de horarios");
+
+                                return PartialView(vm);
+                            }
+                        }
+                    }
+                    var turnoDto = CargarDatos(vm);
+
+                    await _turnoServicio.Update(turnoDto);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+                return PartialView(vm);
+            }
+
+            return volver == 0 || volver == null ? RedirectToAction("TurnosParcial", "Paciente", new { id = vm.PacienteId }) : RedirectToAction("About", "Home");
+        }
+
         // GET: Turno/Delete/5
         [Authorize(Roles = "Administrador")]
         public async Task<ActionResult> Delete(long? id)
@@ -269,6 +342,46 @@ namespace NutricionWeb.Controllers.Turno
             return RedirectToAction("Index");
         }
 
+        [Authorize(Roles = "Administrador")]
+        public async Task<ActionResult> DeleteParcial(long? id)
+        {
+            if (id == null) return RedirectToAction("Error", "Home");
+
+            var turno = await _turnoServicio.GetById(id.Value);
+
+            return PartialView(new TurnoViewModel()
+            {
+                Id = turno.Id,
+                PacienteId = turno.PacienteId,
+                PacienteStr = turno.PacienteStr,
+                HorarioEntrada = turno.HorarioEntrada,
+                HorarioSalida = turno.HorarioSalida,
+                Motivo = turno.Motivo,
+                Numero = turno.Numero,
+                Eliminado = turno.Eliminado
+            });
+        }
+
+        // POST: Turno/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteParcial(TurnoViewModel vm)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    await _turnoServicio.Delete(vm.Id);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+                return PartialView(vm);
+            }
+            return RedirectToAction("TurnosParcial", "Paciente", new { id = vm.PacienteId });
+        }
+
         // GET: Turno/Details/5
         public async Task<ActionResult> Details(long? id)
         {
@@ -277,6 +390,25 @@ namespace NutricionWeb.Controllers.Turno
             var turno = await _turnoServicio.GetById(id.Value);
 
             return View(new TurnoViewModel()
+            {
+                Id = turno.Id,
+                PacienteId = turno.PacienteId,
+                PacienteStr = turno.PacienteStr,
+                HorarioEntrada = turno.HorarioEntrada,
+                HorarioSalida = turno.HorarioSalida,
+                Motivo = turno.Motivo,
+                Numero = turno.Numero,
+                Eliminado = turno.Eliminado
+            });
+        }
+
+        public async Task<ActionResult> DetailsParcial(long? id)
+        {
+            if (id == null) return RedirectToAction("Error", "Home");
+
+            var turno = await _turnoServicio.GetById(id.Value);
+
+            return PartialView(new TurnoViewModel()
             {
                 Id = turno.Id,
                 PacienteId = turno.PacienteId,
