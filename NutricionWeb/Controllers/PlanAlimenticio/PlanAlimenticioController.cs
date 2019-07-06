7748b7445b170b9f4f8dc88eb4bde4d40c7d9f9c
Ezequiel <ezequieltosi.96@hotmail.com>
2019-07-06 00:22:47 -0300

    cuadrito choto en plan

--- NutricionWeb/Controllers/PlanAlimenticio/PlanAlimenticioController.cs
+++ NutricionWeb/Controllers/PlanAlimenticio/PlanAlimenticioController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using NutricionWeb.Helpers.SubGrupo;
 using NutricionWeb.Models.Comida;
 using NutricionWeb.Models.ComidaDetalle;
 using NutricionWeb.Models.Dia;
@@ -6,24 +7,13 @@ using NutricionWeb.Models.Paciente;
 using NutricionWeb.Models.PlanAlimenticio;
 using PagedList;
 using Rotativa;
-using Servicio.Interface.Alimento;
 using Servicio.Interface.Dia;
-using Servicio.Interface.Opcion;
 using Servicio.Interface.Paciente;
 using Servicio.Interface.PlanAlimenticio;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
-using System.Web.Security;
-using NutricionWeb.Helpers.SubGrupo;
-using Servicio.Alimento;
-using Servicio.Comida;
-using Servicio.ComidaDetalle;
-using Servicio.Dia;
-using Servicio.Opcion;
-using Servicio.Paciente;
-using Servicio.PlanAlimenticio;
 using static NutricionWeb.Helpers.PagedList;
 
 namespace NutricionWeb.Controllers.PlanAlimenticio
@@ -91,7 +81,7 @@ namespace NutricionWeb.Controllers.PlanAlimenticio
         }
 
         [Authorize(Roles = "Administrador")]
-        public async Task<ActionResult> DuplicarPlan(int? volver,long? planId)
+        public async Task<ActionResult> DuplicarPlan(int? volver, long? planId)
         {
             ViewBag.Volver = volver ?? 0;
             ViewBag.PlanId = planId ?? 0;
@@ -118,7 +108,7 @@ namespace NutricionWeb.Controllers.PlanAlimenticio
 
             if (volver == 1)
             {
-                return RedirectToAction("ExportarPlanOrdenado", new {id = planId});
+                return RedirectToAction("ExportarPlanOrdenado", new { id = planId });
             }
             return RedirectToAction("Index");
         }
@@ -488,6 +478,8 @@ namespace NutricionWeb.Controllers.PlanAlimenticio
                 ViewBag.CarboPorce = 0;
                 ViewBag.GrasaPorce = 0;
                 ViewBag.ProtePorce = 0;
+
+                ViewBag.CarboGramo = 0;
             }
             else
             {
@@ -498,9 +490,13 @@ namespace NutricionWeb.Controllers.PlanAlimenticio
                 ViewBag.CarboPorce = await _planAlimenticioServicio.CalculateTotalCaloriesCarbos(id.Value) * 100 / plan.TotalCalorias;
                 ViewBag.GrasaPorce = await _planAlimenticioServicio.CalculateTotalCaloriesGrasas(id.Value) * 100 / plan.TotalCalorias;
                 ViewBag.ProtePorce = await _planAlimenticioServicio.CalculateTotalCaloriesProtes(id.Value) * 100 / plan.TotalCalorias;
+
+                ViewBag.CarboGramo = await _planAlimenticioServicio.CalculateTotalCaloriesCarbos(id.Value) / 4;
+                ViewBag.ProteGramo = await _planAlimenticioServicio.CalculateTotalCaloriesProtes(id.Value) / 4;
+                ViewBag.GrasaGramo = await _planAlimenticioServicio.CalculateTotalCaloriesGrasas(id.Value) / 9;
             }
 
-          
+
 
             var comidasVm = Mapper.Map<PlanAlimenticioVistaViewModel>(comidas);
 
@@ -588,7 +584,6 @@ namespace NutricionWeb.Controllers.PlanAlimenticio
         public async Task<ActionResult> BuscarPlan(int? page, string cadenaBuscar)
         {
             var pageNumber = page ?? 1;
-            var eliminado = false;
             var planes =
                 await _planAlimenticioServicio.Get(false, !string.IsNullOrEmpty(cadenaBuscar) ? cadenaBuscar : string.Empty);
 
@@ -623,7 +618,7 @@ namespace NutricionWeb.Controllers.PlanAlimenticio
             };
         }
 
-        public async Task<ActionResult> ModificarRecetario(string recetario , long planId)
+        public async Task<ActionResult> ModificarRecetario(string recetario, long planId)
         {
             try
             {
@@ -633,9 +628,9 @@ namespace NutricionWeb.Controllers.PlanAlimenticio
             }
             catch (Exception ex)
             {
-                return Json(new { estado = false , mensaje = ex.Message });
+                return Json(new { estado = false, mensaje = ex.Message });
             }
-            return Json(new {estado = true });
+            return Json(new { estado = true });
         }
     }
 }
