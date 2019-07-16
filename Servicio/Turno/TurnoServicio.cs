c5db99ea7b63470110ba06886bafb77f70f3773c
seba homsi <sebahomsi@gmail.com>
2019-07-16 10:41:48 -0300

    cambio de first por last para traer el mas proximo

--- Servicio/Turno/TurnoServicio.cs
+++ Servicio/Turno/TurnoServicio.cs
@@ -117,7 +117,7 @@ namespace Servicio.Turno
 
             //var ultimo = turnos.OrderByDescending(t => t.HorarioEntrada).Last(x => x.HorarioEntrada > DateTime.Now);
             var ordenados = turnos.Where(x=> x.Eliminado == false).OrderByDescending(t => t.HorarioEntrada);
-            var ultimo = ordenados.Any(x=>x.HorarioEntrada>DateTime.Now)? ordenados.First(x => x.HorarioEntrada > DateTime.Now):null;
+            var ultimo = ordenados.Any(x=>x.HorarioEntrada>DateTime.Now)? ordenados.Last(x => x.HorarioEntrada > DateTime.Now):null;
             return ultimo;
         }
 
